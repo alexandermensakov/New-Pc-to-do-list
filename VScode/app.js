d32a08f86b548d608196c319ba69ddb49707bc4f
alexandermensakov <90188089+alexandermensakov@users.noreply.github.com>
2022-04-08 14:51:11 +0300

    Add files via upload

---
+++ VScode/app.js
@@ -0,0 +1,64 @@
+document.getElementById("remove-button").onclick = function (){
+    document.getElementById("text1").style.textDecoration = "line-through" 
+}
+document.getElementById("remove-button2").onclick = function (){
+    document.getElementById("text2").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button3").onclick = function (){
+    document.getElementById("text3").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button4").onclick = function (){
+    document.getElementById("text4").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button5").onclick = function (){
+    document.getElementById("text5").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button6").onclick = function (){
+    document.getElementById("text6").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button7").onclick = function (){
+    document.getElementById("text7").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button8").onclick = function (){
+    document.getElementById("text8").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button9").onclick = function (){
+    document.getElementById("text9").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button10").onclick = function (){
+    document.getElementById("text10").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button11").onclick = function (){
+    document.getElementById("text11").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button12").onclick = function (){
+    document.getElementById("text12").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button13").onclick = function (){
+    document.getElementById("text13").style.textDecoration = "line-through" 
+
+}
+
+document.getElementById("remove-button14").onclick = function (){
+    document.getElementById("text14").style.textDecoration = "line-through" 
+
+}
+document.getElementById("remove-button15").onclick = function (){
+    document.getElementById("text15").style.textDecoration = "line-through" 
+
+}
+
+
+
+
